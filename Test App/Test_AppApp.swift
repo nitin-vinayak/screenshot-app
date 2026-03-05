6fb74a31a59eee66f4c3f771fb599cb8c5bf6031
Nitin Vinayak <nitinvinayak.m@gmail.com>
2026-03-05 13:19:09 +0530

    Initial Commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Nitin Vinayak on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
